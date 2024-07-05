5737fdf86bfac9ca8bd7d0fe3dd04ee8d283b01e
sleekgeneral <119498250+sleekgeneral@users.noreply.github.com>
2024-07-06 01:49:43 +0530

    Add files via upload

---
+++ parameter.v
@@ -0,0 +1,17 @@
+`define INPUTFILENAME		 "input.hex" // Input file name
+`define OUTPUTFILENAME		 "test.bmp"		// Output file name
+
+// Choose the operation of code 
+
+//`define MOTION_BLUR_OPERATION;
+//`define SOBEL_OPERATION
+//`define BLURRING_OPERATION
+`define SHARPNESS_OPERATION
+//`define EMBOSS_OPERATION
+//`define BLUE_FILTER_OPERATION
+//`define GREEN_FILTER_OPERATION
+//`define BRIGHTNESS_OPERATION
+//`define RED_FILTER_OPERATION
+//`define INVERT_OPERATION
+//`define BLACKandWHITE_OPERATION
+//`define THRESHOLD_OPERATION 
